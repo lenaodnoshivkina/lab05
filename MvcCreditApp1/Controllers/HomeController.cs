f79db0cad91720150e95b7794ddd72316a282b86
Elena <elena.odnoshivkina@mail.ru>
2021-04-03 03:55:25 +0300

    comlete

---
+++ MvcCreditApp1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCreditApp1.Models;
+
+namespace MvcCreditApp1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CreditContext db = new CreditContext();
+
+
+
+        public ActionResult Index()
+        {
+            var allCredits = db.Credits.ToList<Credit>();
+            ViewBag.Credits = allCredits;
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ MvcCreditApp1/MvcCreditApp1/Models/Credit.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace MvcCreditApp1.Models
+{
+    public class Credit
+    {
+        // ID кредита
+        public virtual int CreditId { get; set; }
+        // Название
+        [DisplayName("Название")]
+        public virtual string Head { get; set; }
+        // Период, на который выдается кредит
+        [DisplayName("Период, на который выдается кредит")]
+        public virtual int Period { get; set; }
+        // Максимальная сумма кредита
+        [DisplayName("Максимальная сумма кредита")]
+        public virtual int Sum { get; set; }
+        // Процентная ставка
+        [DisplayName("Процентная ставка")]
+        public virtual int Procent { get; set; }
+    }
+}
